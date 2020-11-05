463c81f462d0db5304d400403bd1a2b70d754274
Agustin Bergomi <agustin.bergomi@gmail.com>
2020-11-05 14:45:23 -0300

    Con algun comentario ya en archivo Java

--- app/src/main/java/com/agustinbergomi/componentesandroid/MainActivity.java
+++ app/src/main/java/com/agustinbergomi/componentesandroid/MainActivity.java
@@ -3,8 +3,17 @@ package com.agustinbergomi.componentesandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    //seteo las animaciones como final porque no van a cambiar
+    //se cargan con el metodo declarado buscando en resources cada compoenente
+    final Animation animtrans = AnimationUtils.loadAnimation(this,R.anim.translate);
+    final Animation animalpha = AnimationUtils.loadAnimation(this,R.anim.alpha);
+    final Animation animscale = AnimationUtils.loadAnimation(this,R.anim.rotate);
+    final Animation animrotate = AnimationUtils.loadAnimation(this,R.anim.rotate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
